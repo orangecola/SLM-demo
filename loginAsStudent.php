9e5e2ea44d8028d287461a305036fba335334a78
Joel <orangecola3@gmail.com>
2019-02-22 05:28:33 +0000

    Removal of old files

--- loginAsStudent.php
+++
@@ -1,7 +0,0 @@
-<?php
-include_once('components/config.php');
-$_SESSION['username'] = 'Student';
-$_SESSION['role'] = 'student';
-$_SESSION['user_ID'] = '2';
-header("Location: question.php?id=4");
- ?>
